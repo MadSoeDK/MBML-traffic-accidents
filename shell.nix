a53ac4a21bdd400540466adf55841942d90ccdd0
MadSoeDK <s215805@dtu.dk>
2025-04-20 14:21:39 +0200

    added nixos file

---
+++ shell.nix
@@ -0,0 +1,20 @@
+let
+  # We pin to a specific nixpkgs commit for reproducibility.
+  # Last updated: 2024-09-03. Check for new commits at https://status.nixos.org.
+  pkgs = import (fetchTarball
+    "https://github.com/NixOS/nixpkgs/archive/6e99f2a27d60.tar.gz") { };
+in pkgs.mkShell {
+  packages = [
+    (pkgs.python3.withPackages (python-pkgs: [
+      # select Python packages here
+    python-pkgs.numpy
+        #python-pkgs.jax
+        #python-pkgs.jaxlib
+    python-pkgs.scipy
+    python-pkgs.seaborn
+    python-pkgs.matplotlib
+    python-pkgs.notebook
+    python-pkgs.scikit-learn
+    ]))
+  ];
+}
